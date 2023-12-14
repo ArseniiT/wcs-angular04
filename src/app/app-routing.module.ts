77ffb16c9dc5e6c5b8c8406e173001cbbe11bf85
arseniit <arseniit@gmail.com>
2023-12-14 11:14:17 +0100

    Realize routing to user-profile and signup pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserProfileComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'user', component: UserProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
